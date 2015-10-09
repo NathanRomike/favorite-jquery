16ccb2b70e599c47adb59222cbc4b0ab43ab5c7e
NathanRomike <nathanromike@gmail.com>
2015-10-09 00:08:24 -0700

    Add scripts to text story, wrote story narrative

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,34 @@
 $(document).ready(function(){
-  $
+  $("#blanks form").submit(function(event){
+    var movieInput = $("input#movie").val();
+    var bookInput = $("input#book").val();
+    var colorInput = $("input#color").val();
+    var songInput = $("input#song").val();
+    var fruitInput = $("input#fruit").val();
+    var sportInput = $("input#sport").val();
+    var gameInput = $("input#game").val();
+    var cityInput = $("input#city").val();
+    var quoteInput = $("input#quote").val();
+    var seasonInput = $("input#season").val();
+    var dogInput = $("input#dog").val();
+    var breakfastInput = $("input#breakfast").val();
+
+    $(".movie").text(movieInput);
+    $(".book").text(bookInput);
+    $(".color").text(colorInput);
+    $(".song").text(songInput);
+    $(".fruit").text(fruitInput);
+    $(".sport").text(sportInput);
+    $(".game").text(gameInput);
+    $(".city").text(cityInput);
+    $(".quote").text(quoteInput);
+    $(".season").text(seasonInput);
+    $(".dog").text(dogInput);
+    $(".breakfast").text(breakfastInput);
+
+    $("#story").show();
+
+    event.preventDefault();
+
+  });
 });
